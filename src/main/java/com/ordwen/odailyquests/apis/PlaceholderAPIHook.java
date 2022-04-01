088c0ad22c67fef1ab53624222ca8e1e70325510
Samuel Espallier <samuel.espallier@etu.u-bordeaux.fr>
2022-04-01 12:09:08 +0200

    Add quests progressions placeholders.

--- src/main/java/com/ordwen/odailyquests/apis/PlaceholderAPIHook.java
+++ src/main/java/com/ordwen/odailyquests/apis/PlaceholderAPIHook.java
@@ -1,6 +1,7 @@
 package com.ordwen.odailyquests.apis;
 
 import com.ordwen.odailyquests.commands.interfaces.PlayerQuestsInterface;
+import com.ordwen.odailyquests.quests.Quest;
 import com.ordwen.odailyquests.quests.player.QuestsManager;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.OfflinePlayer;
@@ -35,6 +36,26 @@ public class PlaceholderAPIHook extends PlaceholderExpansion {
         if (params.equalsIgnoreCase("drawin")) {
             return PlayerQuestsInterface.timeRemain(player.getName());
         }
+        if (params.equalsIgnoreCase("progress_1")) {
+            return String.valueOf(getQuestProgression(0, player.getName()));
+        }
+        if (params.equalsIgnoreCase("progress_2")) {
+            return String.valueOf(getQuestProgression(1, player.getName()));
+        }
+        if (params.equalsIgnoreCase("progress_3")) {
+            return String.valueOf(getQuestProgression(2, player.getName()));
+        }
         return null;
+    }
+
+    public int getQuestProgression(int index, String playerName) {
+        int i = 0;
+        for (Quest quest : QuestsManager.getActiveQuests().get(playerName).getPlayerQuests().keySet()) {
+            if (i == index) {
+                return QuestsManager.getActiveQuests().get(playerName).getPlayerQuests().get(quest).getProgression();
+            }
+            i++;
+        }
+        return -1;
     }
 }
