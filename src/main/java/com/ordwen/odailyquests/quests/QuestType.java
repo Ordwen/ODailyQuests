e18df0dab4b68d7e8efae4b7066ec63f0a74bcab
Ordwen <samuelespallier77@gmail.com>
2022-07-23 17:50:46 +0200

    suppress useless stuff

--- src/main/java/com/ordwen/odailyquests/quests/QuestType.java
+++ src/main/java/com/ordwen/odailyquests/quests/QuestType.java
@@ -4,44 +4,27 @@ package com.ordwen.odailyquests.quests;
  * List of all possible quests types
  */
 public enum QuestType {
-    BREAK("BREAK"),
-    PLACE("PLACE"),
-    CRAFT("CRAFT"),
-    PICKUP("PICKUP"),
-    LAUNCH("LAUNCH"),
-    CONSUME("CONSUME"),
-    GET("GET"),
-    COOK("COOK"),
-    ENCHANT("ENCHANT"),
-    KILL("KILL"),
-    FISH("FISH"),
-    TAME("TAME"),
-    BREED("BREED"),
-    SHEAR("SHEAR"),
-    MILKING("MILKING"),
+    BREAK,
+    PLACE,
+    CRAFT,
+    PICKUP,
+    LAUNCH,
+    CONSUME,
+    GET,
+    COOK,
+    ENCHANT,
+    KILL,
+    FISH,
+    TAME,
+    BREED,
+    SHEAR,
+    MILKING,
 
-    EXP_POINTS("EXP_POINTS"),
-    EXP_LEVELS("EXP_LEVELS"),
-    VILLAGER_TRADE("VILLAGER_TRADE"),
+    EXP_POINTS,
+    EXP_LEVELS,
+    VILLAGER_TRADE,
 
-    CUSTOM_MOBS("CUSTOM_MOBS"),
-    ;
+    CUSTOM_MOBS,
 
-    private final String typeName;
 
-    /**
-     * Type constructor.
-     * @param typeName the name of the type.
-     */
-    QuestType(String typeName) {
-        this.typeName = typeName;
-    }
-
-    /**
-     * Get the user-configured name for a type.
-     * @return the name of the type.
-     */
-    public String getTypeName() {
-        return this.typeName;
-    }
 }
