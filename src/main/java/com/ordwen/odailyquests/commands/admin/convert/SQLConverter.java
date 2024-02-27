068766cd1eab3a4cce6442795df638611e6260db
Ordwen <samuelespallier77@gmail.com>
2024-02-27 22:27:50 +0100

    refactoring

---
+++ src/main/java/com/ordwen/odailyquests/commands/admin/convert/SQLConverter.java
@@ -0,0 +1,82 @@
+package com.ordwen.odailyquests.commands.admin.convert;
+
+import com.ordwen.odailyquests.quests.player.PlayerQuests;
+import com.ordwen.odailyquests.quests.player.progression.Progression;
+import com.ordwen.odailyquests.quests.player.progression.QuestLoaderUtils;
+import com.ordwen.odailyquests.quests.player.progression.storage.sql.SQLManager;
+import com.ordwen.odailyquests.quests.types.AbstractQuest;
+import com.ordwen.odailyquests.tools.PluginLogger;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.LinkedHashMap;
+
+public abstract class SQLConverter {
+
+    /**
+     * Convert the progression file to the SQL database.
+     *
+     * @param progressionFile the progression file.
+     * @param sqlManager      the SQL manager.
+     */
+    protected void convertData(FileConfiguration progressionFile, SQLManager sqlManager) {
+        long timestamp;
+        int achievedQuests;
+        int totalAchievedQuests;
+        PlayerQuests playerQuests;
+
+        final LinkedHashMap<AbstractQuest, Progression> quests = new LinkedHashMap<>();
+
+        for (String playerName : progressionFile.getKeys(false)) {
+            final ConfigurationSection playerSection = progressionFile.getConfigurationSection(playerName);
+            if (playerSection == null) {
+                error("SQLConverter, 31 - The player section is null.");
+                return;
+            }
+
+            timestamp = playerSection.getLong(".timestamp");
+            achievedQuests = playerSection.getInt(".achievedQuests");
+            totalAchievedQuests = playerSection.getInt(".totalAchievedQuests");
+
+            final ConfigurationSection questsSection = playerSection.getConfigurationSection(".quests");
+            if (questsSection == null) {
+                error("SQLConverter, 36 - The quests section is null.");
+                return;
+            }
+
+            for (String string : questsSection.getKeys(false)) {
+                final ConfigurationSection progressionSection = questsSection.getConfigurationSection(string);
+                if (progressionSection == null) {
+                    error("SQLConverter, 48 - The progression section is null.");
+                    return;
+                }
+
+                int questIndex = progressionSection.getInt(".index");
+                int advancement = progressionSection.getInt(".progression");
+                boolean isAchieved = progressionSection.getBoolean(".isAchieved");
+
+                final Progression progression = new Progression(advancement, isAchieved);
+                final AbstractQuest quest = QuestLoaderUtils.findQuest(playerName, questIndex, Integer.parseInt(string));
+
+                quests.put(quest, progression);
+            }
+
+            playerQuests = new PlayerQuests(timestamp, quests);
+            playerQuests.setAchievedQuests(achievedQuests);
+            playerQuests.setTotalAchievedQuests(totalAchievedQuests);
+
+            sqlManager.getSaveProgressionSQL().saveProgression(playerName, playerQuests, true);
+        }
+    }
+
+    /**
+     * Log an error message.
+     *
+     * @param message the error message.
+     */
+    private void error(String message) {
+        PluginLogger.error("An error occurred while converting YAML to SQL.");
+        PluginLogger.error("If the error persists, please report it to the developer.");
+        PluginLogger.error(message);
+    }
+}
--- src/main/java/com/ordwen/odailyquests/commands/admin/convert/YAMLtoMySQLConverter.java
+++ src/main/java/com/ordwen/odailyquests/commands/admin/convert/YAMLtoMySQLConverter.java
@@ -1,20 +1,14 @@
 package com.ordwen.odailyquests.commands.admin.convert;
 
 import com.ordwen.odailyquests.ODailyQuests;
-import com.ordwen.odailyquests.quests.types.AbstractQuest;
 import com.ordwen.odailyquests.files.ProgressionFile;
-import com.ordwen.odailyquests.quests.player.PlayerQuests;
-import com.ordwen.odailyquests.quests.player.progression.Progression;
-import com.ordwen.odailyquests.quests.player.progression.QuestLoaderUtils;
 import com.ordwen.odailyquests.quests.player.progression.storage.sql.SQLManager;
 import com.ordwen.odailyquests.quests.player.progression.storage.sql.mysql.MySQLManager;
 import com.ordwen.odailyquests.tools.PluginLogger;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 
-import java.util.LinkedHashMap;
-
-public class YAMLtoMySQLConverter {
+public class YAMLtoMySQLConverter extends SQLConverter {
 
     public boolean convert(ODailyQuests oDailyQuests) {
 
@@ -24,37 +18,7 @@ public class YAMLtoMySQLConverter {
                 final FileConfiguration progressionFile = ProgressionFile.getProgressionFileConfiguration();
                 final SQLManager sqlManager = new MySQLManager(ODailyQuests.INSTANCE);
 
-                /* init variables */
-                long timestamp;
-                int achievedQuests;
-                int totalAchievedQuests;
-                PlayerQuests playerQuests;
-
-                final LinkedHashMap<AbstractQuest, Progression> quests = new LinkedHashMap<>();
-
-                for (String playerName : progressionFile.getKeys(false)) {
-                    timestamp = progressionFile.getConfigurationSection(playerName).getLong(".timestamp");
-                    achievedQuests = progressionFile.getConfigurationSection(playerName).getInt(".achievedQuests");
-                    totalAchievedQuests = progressionFile.getConfigurationSection(playerName).getInt(".totalAchievedQuests");
-
-                    for (String string : progressionFile.getConfigurationSection(playerName + ".quests").getKeys(false)) {
-                        int questIndex = progressionFile.getConfigurationSection(playerName + ".quests." + string).getInt(".index");
-                        int advancement = progressionFile.getConfigurationSection(playerName + ".quests." + string).getInt(".progression");
-                        boolean isAchieved = progressionFile.getConfigurationSection(playerName + ".quests." + string).getBoolean(".isAchieved");
-
-                        Progression progression = new Progression(advancement, isAchieved);
-                        AbstractQuest quest = QuestLoaderUtils.findQuest(playerName, questIndex, Integer.parseInt(string));
-
-                        quests.put(quest, progression);
-                    }
-
-                    playerQuests = new PlayerQuests(timestamp, quests);
-                    playerQuests.setAchievedQuests(achievedQuests);
-                    playerQuests.setTotalAchievedQuests(totalAchievedQuests);
-
-                    sqlManager.getSaveProgressionSQL().saveProgression(playerName, playerQuests, true);
-                }
-
+                convertData(progressionFile, sqlManager);
             });
         } catch (Exception e) {
             PluginLogger.error("An error occurred while converting YAML to MySQL.");
