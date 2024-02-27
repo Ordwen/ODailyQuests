19472bacfb040f9c3b26f2524937dacc52aadd19
Ordwen <samuelespallier77@gmail.com>
2024-02-27 23:10:42 +0100

    refactoring

--- src/main/java/com/ordwen/odailyquests/commands/player/handlers/ShowCommand.java
+++ src/main/java/com/ordwen/odailyquests/commands/player/handlers/ShowCommand.java
@@ -25,68 +25,80 @@ public class ShowCommand extends PCommandHandler {
             return;
         }
 
-        switch (args[1]) {
+        if (args[1].equalsIgnoreCase("global")) {
+            openGlobal();
+        } else openCategory(args[1]);
+    }
 
-            case "global" -> {
-                if (Modes.getQuestsMode() == 2) {
-                    categorizedEnabled(player);
-                    return;
-                }
+    /**
+     * Opens the category interface.
+     * @param category the category.
+     */
+    private void openCategory(String category) {
+        if (Modes.getQuestsMode() == 1) {
+            categorizedDisabled(player);
+            return;
+        }
 
-                if (!player.hasPermission(QuestsPermissions.QUESTS_SHOW_GLOBAL.getPermission())) {
-                    noPermissionCategory(player);
-                    return;
-                }
-                final Inventory inventory = InterfacesManager.getInterfaceFirstPage("global", player);
-                player.openInventory(inventory);
-            }
-
-            case "easy" -> {
-                if (Modes.getQuestsMode() == 1) {
-                    categorizedDisabled(player);
-                    return;
-                }
-
-                if (!player.hasPermission(QuestsPermissions.QUESTS_SHOW_EASY.getPermission())) {
-                    noPermissionCategory(player);
-                    return;
-                }
-                final Inventory inventory = InterfacesManager.getInterfaceFirstPage("easy", player);
-                player.openInventory(inventory);
-
-            }
-
-            case "medium" -> {
-                if (Modes.getQuestsMode() == 1) {
-                    categorizedDisabled(player);
-                    return;
-                }
-
-                if (!player.hasPermission(QuestsPermissions.QUESTS_SHOW_MEDIUM.getPermission())) {
-                    noPermissionCategory(player);
-                    return;
-                }
-                final Inventory inventory = InterfacesManager.getInterfaceFirstPage("medium", player);
-                player.openInventory(inventory);
-
-            }
-
-            case "hard" -> {
-                if (Modes.getQuestsMode() == 1) {
-                    categorizedDisabled(player);
-                    return;
-                }
-
-                if (!player.hasPermission(QuestsPermissions.QUESTS_SHOW_HARD.getPermission())) {
-                    noPermissionCategory(player);
-                    return;
-                }
-                final Inventory inventory = InterfacesManager.getInterfaceFirstPage("hard", player);
-                player.openInventory(inventory);
-
-            }
-
+        switch (category) {
+            case "global" -> openGlobal();
+            case "easy" -> openEasy();
+            case "medium" -> openMedium();
+            case "hard" -> openHard();
             default -> invalidCategory(player);
         }
+    }
+
+    /**
+     * Opens the global interface.
+     */
+    private void openGlobal() {
+        if (Modes.getQuestsMode() == 2) {
+            categorizedEnabled(player);
+            return;
+        }
+
+        if (!player.hasPermission(QuestsPermissions.QUESTS_SHOW_GLOBAL.getPermission())) {
+            noPermissionCategory(player);
+            return;
+        }
+        final Inventory inventory = InterfacesManager.getInterfaceFirstPage("global", player);
+        player.openInventory(inventory);
+    }
+
+    /**
+     * Opens the easy interface.
+     */
+    private void openEasy() {
+        if (!player.hasPermission(QuestsPermissions.QUESTS_SHOW_EASY.getPermission())) {
+            noPermissionCategory(player);
+            return;
+        }
+        final Inventory inventory = InterfacesManager.getInterfaceFirstPage("easy", player);
+        player.openInventory(inventory);
+    }
+
+    /**
+     * Opens the medium interface.
+     */
+    private void openMedium() {
+        if (!player.hasPermission(QuestsPermissions.QUESTS_SHOW_MEDIUM.getPermission())) {
+            noPermissionCategory(player);
+            return;
+        }
+        final Inventory inventory = InterfacesManager.getInterfaceFirstPage("medium", player);
+        player.openInventory(inventory);
+    }
+
+    /**
+     * Opens the hard interface.
+     */
+    private void openHard() {
+        if (!player.hasPermission(QuestsPermissions.QUESTS_SHOW_HARD.getPermission())) {
+            noPermissionCategory(player);
+            return;
+        }
+        final Inventory inventory = InterfacesManager.getInterfaceFirstPage("hard", player);
+        player.openInventory(inventory);
     }
 }
