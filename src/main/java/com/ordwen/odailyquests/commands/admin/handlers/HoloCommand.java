90d2c0c1a5db2709a61d07ea123bbf89ce913e12
Ordwen <samuelespallier77@gmail.com>
2024-02-27 22:47:32 +0100

    refactoring

--- src/main/java/com/ordwen/odailyquests/commands/admin/handlers/HoloCommand.java
+++ src/main/java/com/ordwen/odailyquests/commands/admin/handlers/HoloCommand.java
@@ -45,46 +45,72 @@ public class HoloCommand extends ACommandHandler {
 
             switch (args[2]) {
                 case "global" -> {
-                    if (!CategoriesLoader.getGlobalQuests().isEmpty()) {
-                        HolographicDisplaysHook.createHologram(index, CategoriesLoader.getGlobalQuests(), ((Player) sender).getPlayer());
-                    } else {
-                        final String msg = QuestsMessages.HOLO_CATEGORIZED_ENABLED.toString();
-                        if (msg != null) sender.sendMessage(msg);
+                    if (CategoriesLoader.getGlobalQuests().isEmpty()) {
+                        enabled();
+                        return;
                     }
+
+                    HolographicDisplaysHook.createHologram(index, CategoriesLoader.getGlobalQuests(), ((Player) sender).getPlayer());
                 }
                 case "easy" -> {
                     if (!CategoriesLoader.getEasyQuests().isEmpty()) {
-                        HolographicDisplaysHook.createHologram(index, CategoriesLoader.getEasyQuests(), ((Player) sender).getPlayer());
-                    } else {
-                        final String msg = QuestsMessages.HOLO_CATEGORIZED_DISABLED.toString();
-                        if (msg != null) sender.sendMessage(msg);
+                        disabled();
+                        return;
                     }
+
+                    HolographicDisplaysHook.createHologram(index, CategoriesLoader.getEasyQuests(), ((Player) sender).getPlayer());
                 }
 
                 case "medium" -> {
                     if (!CategoriesLoader.getMediumQuests().isEmpty()) {
-                        HolographicDisplaysHook.createHologram(index, CategoriesLoader.getMediumQuests(), ((Player) sender).getPlayer());
-                    } else {
-                        final String msg = QuestsMessages.HOLO_CATEGORIZED_DISABLED.toString();
-                        if (msg != null) sender.sendMessage(msg);
+                        disabled();
+                        return;
                     }
+
+                    HolographicDisplaysHook.createHologram(index, CategoriesLoader.getMediumQuests(), ((Player) sender).getPlayer());
                 }
 
                 case "hard" -> {
-                    if (!CategoriesLoader.getHardQuests().isEmpty()) {
-                        HolographicDisplaysHook.createHologram(index, CategoriesLoader.getHardQuests(), ((Player) sender).getPlayer());
-                    } else {
-                        final String msg = QuestsMessages.HOLO_CATEGORIZED_DISABLED.toString();
-                        if (msg != null) sender.sendMessage(msg);
+                    if (CategoriesLoader.getHardQuests().isEmpty()) {
+                        disabled();
+                        return;
                     }
+
+                    HolographicDisplaysHook.createHologram(index, CategoriesLoader.getHardQuests(), ((Player) sender).getPlayer());
                 }
 
                 default -> {
-                    final String msg = QuestsMessages.INVALID_CATEGORY.toString();
-                    if (msg != null) sender.sendMessage(msg);
+                    invalidCategory();
+                    return;
                 }
             }
-        } else help();
+        }
+
+        help();
+    }
+
+    /**
+     * Send a message to the sender if categorized quests are disabled.
+     */
+    private void disabled() {
+        final String msg = QuestsMessages.HOLO_CATEGORIZED_DISABLED.toString();
+        if (msg != null) sender.sendMessage(msg);
+    }
+
+    /**
+     * Send a message to the sender if categorized quests are enabled.
+     */
+    private void enabled() {
+        final String msg = QuestsMessages.HOLO_CATEGORIZED_ENABLED.toString();
+        if (msg != null) sender.sendMessage(msg);
+    }
+
+    /**
+     * Send a message to the sender if the category is invalid.
+     */
+    private void invalidCategory() {
+        final String msg = QuestsMessages.INVALID_CATEGORY.toString();
+        if (msg != null) sender.sendMessage(msg);
     }
 
     /**
