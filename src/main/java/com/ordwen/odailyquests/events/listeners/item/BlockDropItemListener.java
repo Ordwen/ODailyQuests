f00dd511cede61a28f98b94b700baf1359c37144
Ordwen <samuelespallier77@gmail.com>
2024-03-10 23:37:57 +0100

    refactoring the farming quest type

--- src/main/java/com/ordwen/odailyquests/events/listeners/item/BlockDropItemListener.java
+++ src/main/java/com/ordwen/odailyquests/events/listeners/item/BlockDropItemListener.java
@@ -7,6 +7,7 @@ import org.bukkit.Material;
 import org.bukkit.block.data.Ageable;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDropItemEvent;
@@ -19,7 +20,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class BlockDropItemListener extends AbstractItemChecker implements Listener {
 
@@ -32,7 +32,10 @@ public class BlockDropItemListener extends AbstractItemChecker implements Listen
                     "MELON",
                     "MELON_SLICE",
                     "TORCHFLOWER",
-                    "KELP_PLANT"
+                    "KELP_PLANT",
+                    "BROWN_MUSHROOM",
+                    "BROWN_MUSHROOM_BLOCK",
+                    "MUSHROOM_STEM"
             ));
 
     @EventHandler
@@ -40,55 +43,26 @@ public class BlockDropItemListener extends AbstractItemChecker implements Listen
 
         if (event.isCancelled()) return;
 
+        final Player player = event.getPlayer();
         final BlockData data = event.getBlockState().getBlockData();
         final Material dataMaterial = data.getMaterial();
 
         // check if the dropped item figure in the non-crops items list
         if (farmableItems.contains(data.getMaterial().toString())) {
 
-            final AtomicBoolean valid = new AtomicBoolean(true);
-
             // check if the block have been placed by the player
             if (dataMaterial.isBlock()) {
                 if (Antiglitch.isStorePlacedBlocks()) {
                     if (!event.getBlock().getMetadata("odailyquests:placed").isEmpty()) {
-                        valid.set(false);
+                        return;
                     }
                 }
             }
 
-            if (!valid.get()) {
-                return;
-            }
-
-            Material material = switch (dataMaterial.toString()) {
-                case "KELP_PLANT" -> Material.valueOf("KELP");
-                default -> dataMaterial;
-            };
-
+            // get the dropped items
             final List<Item> drops = event.getItems();
             if (drops.isEmpty()) return;
-
-            boolean isSlicedMelon = false;
-            int amount = 0;
-            for (Item item : drops) {
-                final Material itemMaterial = item.getItemStack().getType();
-
-                if (itemMaterial == Material.MELON_SLICE) isSlicedMelon = true;
-
-                if (isSlicedMelon) {
-                    if (itemMaterial == Material.MELON_SLICE) {
-                        amount += item.getItemStack().getAmount();
-                    }
-                }
-
-                else if (itemMaterial == material) {
-                    amount += item.getItemStack().getAmount();
-                }
-            }
-
-            if (isSlicedMelon) setPlayerQuestProgression(event.getPlayer(), new ItemStack(Material.MELON_SLICE), amount, QuestType.FARMING);
-            else setPlayerQuestProgression(event.getPlayer(), new ItemStack(material), amount, QuestType.FARMING);
+            handleDrops(player, drops);
         }
 
         // check if the dropped item is a crop
@@ -96,31 +70,10 @@ public class BlockDropItemListener extends AbstractItemChecker implements Listen
 
             if (ageable.getAge() == ageable.getMaximumAge()) {
 
-                Material material = switch (data.getMaterial().toString()) {
-                    case "POTATOES" -> Material.valueOf("POTATO");
-                    case "CARROTS" -> Material.valueOf("CARROT");
-                    case "BEETROOTS" -> Material.valueOf("BEETROOT");
-                    case "COCOA" -> Material.valueOf("COCOA_BEANS");
-                    case "SWEET_BERRY_BUSH" -> Material.valueOf("SWEET_BERRIES");
-                    case "PITCHER_CROP" -> Material.valueOf("PITCHER_PLANT");
-                    default -> dataMaterial;
-                };
-
+                // get the dropped items
                 final List<Item> drops = event.getItems();
-
-                int amount = 0;
-                for (Item item : drops) {
-
-                    if (item.getItemStack().getType() == material) {
-                        amount += item.getItemStack().getAmount();
-                    }
-                }
-
-                if (amount > 0) {
-
-
-                    setPlayerQuestProgression(event.getPlayer(), new ItemStack(material), amount, QuestType.FARMING);
-                }
+                if (drops.isEmpty()) return;
+                handleDrops(player, drops);
             }
         }
 
@@ -131,12 +84,29 @@ public class BlockDropItemListener extends AbstractItemChecker implements Listen
                 for (Item item : event.getItems()) {
                     final ItemStack drop = item.getItemStack();
                     final ItemMeta dropMeta = drop.getItemMeta();
-                    final PersistentDataContainer pdc = dropMeta.getPersistentDataContainer();
+                    if (dropMeta == null) continue;
 
+                    final PersistentDataContainer pdc = dropMeta.getPersistentDataContainer();
                     pdc.set(Antiglitch.BROKEN_KEY, PersistentDataType.STRING, event.getPlayer().getUniqueId().toString());
                     drop.setItemMeta(dropMeta);
                 }
             }
+        }
+    }
+
+    /**
+     * Handle the dropped items and update the player progression.
+     *
+     * @param player involved player in the event
+     * @param drops list of dropped items
+     */
+    private void handleDrops(Player player, List<Item> drops) {
+        for (Item item : drops) {
+            final ItemStack droppedItem = item.getItemStack();
+            final Material droppedMaterial = droppedItem.getType();
+
+            System.out.println("Dropped item: " + droppedMaterial + " - " + droppedItem.getAmount());
+            setPlayerQuestProgression(player, new ItemStack(droppedMaterial), droppedItem.getAmount(), QuestType.FARMING);
         }
     }
 }
