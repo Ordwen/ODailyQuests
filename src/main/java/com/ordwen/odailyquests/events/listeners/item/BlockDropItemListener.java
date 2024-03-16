44afed98ba522276acc7291c1c3cf6dc02a89601
Ordwen <samuelespallier77@gmail.com>
2024-03-16 19:07:37 +0100

    refactoring

--- src/main/java/com/ordwen/odailyquests/events/listeners/item/BlockDropItemListener.java
+++ src/main/java/com/ordwen/odailyquests/events/listeners/item/BlockDropItemListener.java
@@ -16,27 +16,9 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class BlockDropItemListener extends AbstractItemChecker implements Listener {
-
-    /* store items that can be farmed but are not counted as crops or ageables */
-    private final Set<String> farmableItems = new HashSet<>(
-            Arrays.asList(
-                    "SUGAR_CANE",
-                    "CACTUS",
-                    "PUMPKIN",
-                    "MELON",
-                    "MELON_SLICE",
-                    "TORCHFLOWER",
-                    "KELP_PLANT",
-                    "BROWN_MUSHROOM",
-                    "BROWN_MUSHROOM_BLOCK",
-                    "MUSHROOM_STEM"
-            ));
 
     @EventHandler
     public void onBlockDropItem(BlockDropItemEvent event) {
@@ -47,35 +29,27 @@ public class BlockDropItemListener extends AbstractItemChecker implements Listen
         final BlockData data = event.getBlockState().getBlockData();
         final Material dataMaterial = data.getMaterial();
 
-        // check if the dropped item figure in the non-crops items list
-        if (farmableItems.contains(data.getMaterial().toString())) {
-
-            // check if the block have been placed by the player
-            if (dataMaterial.isBlock()) {
-                if (Antiglitch.isStorePlacedBlocks()) {
-                    if (!event.getBlock().getMetadata("odailyquests:placed").isEmpty()) {
-                        return;
-                    }
-                }
-            }
-
-            // get the dropped items
-            final List<Item> drops = event.getItems();
-            if (drops.isEmpty()) return;
-            handleDrops(player, drops);
-        }
+        final List<Item> drops = event.getItems();
+        if (drops.isEmpty()) return;
 
         // check if the dropped item is a crop
-        else if (data instanceof Ageable ageable) {
-
+        if (data instanceof Ageable ageable) {
             if (ageable.getAge() == ageable.getMaximumAge()) {
-
-                // get the dropped items
-                final List<Item> drops = event.getItems();
-                if (drops.isEmpty()) return;
                 handleDrops(player, drops);
+                return;
             }
         }
+
+        // check if the block have been placed by the player
+        if (dataMaterial.isBlock()) {
+            if (Antiglitch.isStorePlacedBlocks()) {
+                if (!event.getBlock().getMetadata("odailyquests:placed").isEmpty()) {
+                    return;
+                }
+            }
+        }
+
+        handleDrops(player, drops);
 
         // check if the dropped item is a block that can be posed
         if (dataMaterial.isBlock()) {
@@ -98,14 +72,13 @@ public class BlockDropItemListener extends AbstractItemChecker implements Listen
      * Handle the dropped items and update the player progression.
      *
      * @param player involved player in the event
-     * @param drops list of dropped items
+     * @param drops  list of dropped items
      */
     private void handleDrops(Player player, List<Item> drops) {
         for (Item item : drops) {
             final ItemStack droppedItem = item.getItemStack();
             final Material droppedMaterial = droppedItem.getType();
 
-            System.out.println("Dropped item: " + droppedMaterial + " - " + droppedItem.getAmount());
             setPlayerQuestProgression(player, new ItemStack(droppedMaterial), droppedItem.getAmount(), QuestType.FARMING);
         }
     }
