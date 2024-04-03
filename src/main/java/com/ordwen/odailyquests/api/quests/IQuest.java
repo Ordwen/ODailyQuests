6470bfa11e96557c6529badfcdb7d41d74e64f82
ses <s.espallier@sciencespobordeaux.fr>
2024-04-03 17:13:38 +0200

    help

--- src/main/java/com/ordwen/odailyquests/api/quests/IQuest.java
+++ src/main/java/com/ordwen/odailyquests/api/quests/IQuest.java
@@ -1,9 +1,10 @@
 package com.ordwen.odailyquests.api.quests;
 
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.event.Event;
 
 public interface IQuest {
 
-    boolean canProgress();
+    boolean canProgress(Event event);
     void loadParameters(ConfigurationSection section, String file, int index);
 }
--- src/main/java/com/ordwen/odailyquests/quests/types/KillQuest.java
+++ src/main/java/com/ordwen/odailyquests/quests/types/KillQuest.java
@@ -1,15 +1,9 @@
 package com.ordwen.odailyquests.quests.types;
 
-import com.ordwen.odailyquests.configuration.essentials.Debugger;
-import com.ordwen.odailyquests.configuration.integrations.WildStackerEnabled;
-import com.ordwen.odailyquests.events.antiglitch.EntitySource;
-import com.ordwen.odailyquests.externs.hooks.mobs.MythicMobsHook;
 import com.ordwen.odailyquests.quests.types.shared.EntityQuest;
-import io.lumine.mythic.bukkit.MythicBukkit;
-import io.lumine.mythic.core.mobs.ActiveMob;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.event.EventHandler;
+import org.bukkit.event.Event;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 
@@ -24,40 +18,29 @@ public class KillQuest extends EntityQuest implements Listener {
     }
 
     @Override
-    public void canProgress(LivingEntity entity) {
-        boolean isRequiredEntity = false;
-        if (requiredEntities == null) isRequiredEntity = true;
-        else {
-            for (EntityType type : requiredEntities) {
-                isRequiredEntity = type.equals(entity.getType());
-                if (isRequiredEntity) break;
+    public boolean canProgress(Event provided) {
+        if (provided instanceof EntityDeathEvent event) {
+            final LivingEntity entity = event.getEntity();
+
+            boolean isRequiredEntity = false;
+            if (requiredEntities == null) isRequiredEntity = true;
+            else {
+                for (EntityType type : requiredEntities) {
+                    isRequiredEntity = type.equals(entity.getType());
+                    if (isRequiredEntity) break;
+                }
             }
+
+            return true;
+
+            /*
+            if (isRequiredEntity) {
+                setPlayerQuestProgression(entity.getKiller(), 1, "KILL");
+            }
+
+             */
         }
 
-        if (isRequiredEntity) {
-            setPlayerQuestProgression(entity.getKiller(), 1, "KILL");
-        }
-    }
-
-    @EventHandler
-    public void onEntityDeathEvent(EntityDeathEvent event) {
-        final LivingEntity entity = event.getEntity();
-
-        if (MythicMobsHook.isMythicMobsSetup()) {
-            final ActiveMob mythicMob = MythicBukkit.inst().getMobManager().getActiveMob(entity.getUniqueId()).orElse(null);
-            if (mythicMob != null) return;
-        }
-
-        final boolean isEntityFromSpawner = EntitySource.isEntityFromSpawner(entity);
-        EntitySource.removeEntityFromSpawner(entity);
-
-        if (WildStackerEnabled.isEnabled()) return;
-        if (entity.getKiller() == null) return;
-        if (isEntityFromSpawner) return;
-
-        Debugger.addDebug("=========================================================================================");
-        Debugger.addDebug("EntityDeathListener: onEntityDeathEvent summoned by " + entity.getKiller().getName() + " for " + entity.getType() + ".");
-
-        progressionHandler(entity);
+        return false;
     }
 }
