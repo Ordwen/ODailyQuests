05cfcaf071274a4292f4faa49b65b29ea9e80fd9
ses <s.espallier@sciencespobordeaux.fr>
2024-04-15 10:59:38 +0200

    added wg check

--- src/main/java/com/ordwen/odailyquests/externs/hooks/Protection.java
+++ src/main/java/com/ordwen/odailyquests/externs/hooks/Protection.java
@@ -4,6 +4,7 @@ import com.ordwen.odailyquests.tools.PluginLogger;
 import com.palmergames.bukkit.towny.object.TownyPermission;
 import com.palmergames.bukkit.towny.utils.PlayerCacheUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
@@ -51,25 +52,36 @@ public class Protection {
     public static boolean canBuild(Player player, Block block) {
         if (!isTownyEnabled() && !isWorldGuardEnabled()) return true;
 
-        // Towny check
-        return PlayerCacheUtil.getCachePermission(player, block.getLocation(), block.getType(), TownyPermission.ActionType.BUILD);
-
-        // WorldGuard check
+        return checkTowny(player, block) && checkWg(player, block);
     }
 
     /**
-     * Check if the player can build at the location.
+     * Check if the player can build at the location using Towny.
      *
-     * @param player   the player
-     * @param world    the world
-     * @param location the location
+     * @param player the player
+     * @param block  the block
      * @return true if the player can build, false otherwise
      */
-    private static boolean checkWg(Player player, World world, Location location) {
+    private static boolean checkTowny(Player player, Block block) {
+        if (!isTownyEnabled()) return true;
+
+        final Location location = block.getLocation();
+        return PlayerCacheUtil.getCachePermission(player, location, block.getType(), TownyPermission.ActionType.BUILD);
+    }
+
+    /**
+     * Check if the player can build at the location using WorldGuard.
+     *
+     * @param player the player
+     * @param block  the block
+     * @return true if the player can build, false otherwise
+     */
+    private static boolean checkWg(Player player, Block block) {
         if (!isWorldGuardEnabled()) return true;
 
+        final Location location = block.getLocation();
         final com.sk89q.worldedit.util.Location adaptedLocation = BukkitAdapter.adapt(location);
-        final com.sk89q.worldedit.world.World adaptedWorld = BukkitAdapter.adapt(world);
+        final com.sk89q.worldedit.world.World adaptedWorld = BukkitAdapter.adapt(location.getWorld());
         final LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
 
         if (wgPlatform.getSessionManager().hasBypass(localPlayer, adaptedWorld)) return true;
