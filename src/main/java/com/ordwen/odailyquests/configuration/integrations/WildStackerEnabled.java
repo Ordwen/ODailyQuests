fe1acff4f5a4bef9d3b81f8f788cb3729377ae3f
Ordwen <samuelespallier77@gmail.com>
2024-04-21 16:27:01 +0200

    refactor: SonarCloud compliance

--- src/main/java/com/ordwen/odailyquests/configuration/integrations/WildStackerEnabled.java
+++ src/main/java/com/ordwen/odailyquests/configuration/integrations/WildStackerEnabled.java
@@ -23,12 +23,16 @@ public class WildStackerEnabled {
      */
     public void loadWildStackerEnabled() {
         final String path = "use_wildstacker";
-        isEnabled = configurationFiles.getConfigFile().getBoolean(path);
+        setEnabled(configurationFiles.getConfigFile().getBoolean(path));
 
         if (Bukkit.getPluginManager().getPlugin("WildStacker") == null) {
             PluginLogger.error("WildStacker is enabled in the config but the plugin is not installed.");
             PluginLogger.error("Disabling WildStacker integration, otherwise KILL quests will not work properly.");
-            isEnabled = false;
+            setEnabled(false);
         }
+    }
+
+    private static void setEnabled(boolean enabled) {
+        isEnabled = enabled;
     }
 }
