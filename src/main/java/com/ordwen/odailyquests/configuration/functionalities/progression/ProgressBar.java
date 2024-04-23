8dcab8061c4059700fd9064ac37bf81056af54aa
ses <s.espallier@sciencespobordeaux.fr>
2024-04-23 16:20:53 +0200

    refactoring: sonar compliance

--- src/main/java/com/ordwen/odailyquests/configuration/functionalities/progression/ProgressBar.java
+++ src/main/java/com/ordwen/odailyquests/configuration/functionalities/progression/ProgressBar.java
@@ -28,18 +28,18 @@ public class ProgressBar {
             PluginLogger.warn("Progress bar section is missing in the configuration file.");
             PluginLogger.warn("Using default progress bar.");
 
-            symbol = "|";
-            completedColor = ChatColor.GREEN.toString();
-            remainingColor = ChatColor.GRAY.toString();
-            amountOfSymbols = 20;
+            setSymbol("|");
+            setCompletedColor(ChatColor.GREEN.toString());
+            setRemainingColor(ChatColor.GRAY.toString());
+            setAmountOfSymbols(20);
 
             return;
         }
 
-        symbol = section.contains("symbol") ? section.getString("symbol") : "|";
-        completedColor = section.contains("completed_color") ? ColorConvert.convertColorCode(section.getString("completed_color")) : ChatColor.GREEN.toString();
-        remainingColor = section.contains("remaining_color") ? ColorConvert.convertColorCode(section.getString("remaining_color")) : ChatColor.GRAY.toString();
-        amountOfSymbols = section.contains("amount_of_symbols") ? section.getInt("amount_of_symbols") : 20;
+        setSymbol(section.contains("symbol") ? section.getString("symbol") : "|");
+        setCompletedColor(section.contains("completed_color") ? ColorConvert.convertColorCode(section.getString("completed_color")) : ChatColor.GREEN.toString());
+        setRemainingColor(section.contains("remaining_color") ? ColorConvert.convertColorCode(section.getString("remaining_color")) : ChatColor.GRAY.toString());
+        setAmountOfSymbols(section.contains("amount_of_symbols") ? section.getInt("amount_of_symbols") : 20);
     }
 
     /**
@@ -64,5 +64,21 @@ public class ProgressBar {
         }
 
         return builder.toString();
+    }
+
+    private static void setSymbol(String symbol) {
+        ProgressBar.symbol = symbol;
+    }
+
+    private static void setCompletedColor(String completedColor) {
+        ProgressBar.completedColor = completedColor;
+    }
+
+    private static void setRemainingColor(String remainingColor) {
+        ProgressBar.remainingColor = remainingColor;
+    }
+
+    private static void setAmountOfSymbols(int amountOfSymbols) {
+        ProgressBar.amountOfSymbols = amountOfSymbols;
     }
 }
