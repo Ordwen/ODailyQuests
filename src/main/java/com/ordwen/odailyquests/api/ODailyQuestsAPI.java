607f547ec0ba9dc9e45a248cd3b40cdd19ae6eeb
ses <s.espallier@sciencespobordeaux.fr>
2024-04-24 16:46:54 +0200

    refactoring: improving API for quest registration

--- src/main/java/com/ordwen/odailyquests/api/ODailyQuestsAPI.java
+++ src/main/java/com/ordwen/odailyquests/api/ODailyQuestsAPI.java
@@ -1,28 +1,27 @@
 package com.ordwen.odailyquests.api;
 
-import com.ordwen.odailyquests.ODailyQuests;
 import com.ordwen.odailyquests.api.quests.QuestTypeRegistry;
 import com.ordwen.odailyquests.quests.player.PlayerQuests;
 import com.ordwen.odailyquests.quests.player.QuestsManager;
+import com.ordwen.odailyquests.quests.types.AbstractQuest;
+import com.ordwen.odailyquests.tools.PluginLogger;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This class is used to access the API of ODailyQuests.
  */
 public class ODailyQuestsAPI {
 
-    private final ODailyQuests oDailyQuests;
+    private static boolean canRegister = true;
+    private static final Map<String, Class<? extends AbstractQuest>> externalTypes = new HashMap<>();
+
     private final QuestTypeRegistry questTypeRegistry = new QuestTypeRegistry();
 
     /**
-     * Constructor for the ODailyQuestsAPI.
-     * @param oDailyQuests main class of the plugin
-     */
-    public ODailyQuestsAPI(ODailyQuests oDailyQuests) {
-        this.oDailyQuests = oDailyQuests;
-    }
-
-    /**
      * Get the QuestTypeRegistry object.
+     *
      * @return QuestTypeRegistry object
      */
     public QuestTypeRegistry getQuestTypeRegistry() {
@@ -31,10 +30,41 @@ public class ODailyQuestsAPI {
 
     /**
      * Get the PlayerQuests object of a player.
+     *
      * @param playerName name of the player
      * @return PlayerQuests object
      */
     public PlayerQuests getPlayerQuests(String playerName) {
         return QuestsManager.getActiveQuests().get(playerName);
+    }
+
+    /**
+     * Register a new quest type.
+     *
+     * @param type       type of the quest
+     * @param questClass class of the quest
+     */
+    public static void registerQuestType(String type, Class<? extends AbstractQuest> questClass) {
+        if (canRegister) {
+            externalTypes.put(type, questClass);
+        } else {
+            PluginLogger.error("Cannot register quest type " + type + " because the plugin has already been enabled.");
+        }
+    }
+
+    /**
+     * Disable the registration of new quest types.
+     */
+    public static void disableRegistration() {
+        canRegister = false;
+    }
+
+    /**
+     * Get the external quest types.
+     *
+     * @return external quest types
+     */
+    public static Map<String, Class<? extends AbstractQuest>> getExternalTypes() {
+        return externalTypes;
     }
 }
