46d0c842b01130b91755c2923b57e6619bdf768c
ses <s.espallier@sciencespobordeaux.fr>
2024-04-29 17:04:13 +0200

    feat: added shared_mobs option for MYTHIC_MOBS quests

--- src/main/java/com/ordwen/odailyquests/events/listeners/entity/custom/mobs/MythicMobDeathListener.java
+++ src/main/java/com/ordwen/odailyquests/events/listeners/entity/custom/mobs/MythicMobDeathListener.java
@@ -1,18 +1,36 @@
 package com.ordwen.odailyquests.events.listeners.entity.custom.mobs;
 
 
+import com.ordwen.odailyquests.configuration.integrations.SharedMobs;
 import com.ordwen.odailyquests.quests.player.progression.PlayerProgressor;
+import io.lumine.mythic.api.adapters.AbstractEntity;
 import io.lumine.mythic.bukkit.events.MythicMobDeathEvent;
+import io.lumine.mythic.core.mobs.ActiveMob;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.Set;
 
 public class MythicMobDeathListener extends PlayerProgressor implements Listener {
 
     @EventHandler
     public void onMythicMobsDeathEvent(MythicMobDeathEvent event) {
+        if (SharedMobs.isEnabled()) {
+            final ActiveMob.ThreatTable threatTable = event.getMob().getThreatTable();
+            final Set<AbstractEntity> targets = threatTable.getAllThreatTargets();
+
+            for (AbstractEntity target : targets) {
+                if (target instanceof Player player) {
+                    setPlayerQuestProgression(event, player, 1, "MYTHIC_MOBS");
+                }
+            }
+
+            return;
+        }
+
         if (event.getKiller() != null && event.getKiller() instanceof Player player) {
-            setPlayerQuestProgression(event, player, 1, "CUSTOM_MOBS");
+            setPlayerQuestProgression(event, player, 1, "MYTHIC_MOBS");
         }
     }
 }
