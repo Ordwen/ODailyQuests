f7a071cfa32c2abe4fcc056144b1d5753b1924fc
Samuel Espallier <s.espallier@sciencespobordeaux.fr>
2025-02-12 10:02:06 +0100

    Added a HIGHEST priority to the BlockBreakEvent to improve compatibility with other plugins that would cancel the event before it was read by ODailyQuests.

--- src/main/java/com/ordwen/odailyquests/events/listeners/item/BlockBreakListener.java
+++ src/main/java/com/ordwen/odailyquests/events/listeners/item/BlockBreakListener.java
@@ -10,12 +10,13 @@ import org.bukkit.block.Block;
 import org.bukkit.block.data.Ageable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
 public class BlockBreakListener extends PlayerProgressor implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onBlockBreakEvent(BlockBreakEvent event) {
 
         Debugger.addDebug("BlockBreakListener: onBlockBreakEvent summoned.");
