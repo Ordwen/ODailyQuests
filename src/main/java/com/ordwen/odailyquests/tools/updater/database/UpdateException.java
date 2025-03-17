8625380357a7090d5bb4a25ec9c9fa399f2fb812
Ordwen <samuelespallier77@gmail.com>
2025-03-17 23:56:54 +0100

    refacto: clean unused classes

--- src/main/java/com/ordwen/odailyquests/tools/updater/database/UpdateException.java
+++
@@ -1,7 +0,0 @@
-package com.ordwen.odailyquests.tools.updater.database;
-
-public class UpdateException extends RuntimeException {
-    public UpdateException(String message) {
-        super(message);
-    }
-}
