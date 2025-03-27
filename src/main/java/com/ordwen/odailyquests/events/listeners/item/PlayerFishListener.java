dec1c02d7537725257f19e4fffd77bef30392707
Ordwen <samuelespallier77@gmail.com>
2025-03-27 18:35:50 +0100

    fix: improve compatibility with fishing plugins

--- src/main/java/com/ordwen/odailyquests/events/listeners/item/PlayerFishListener.java
+++ src/main/java/com/ordwen/odailyquests/events/listeners/item/PlayerFishListener.java
@@ -5,17 +5,22 @@ import com.ordwen.odailyquests.configuration.essentials.Debugger;
 import com.ordwen.odailyquests.quests.player.progression.PlayerProgressor;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 
 public class PlayerFishListener extends PlayerProgressor implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerFishEvent(PlayerFishEvent event) {
-        if (event.isCancelled()) return;
+        if (event.isCancelled()) {
+            Debugger.write("PlayerFishListener: onPlayerFishEvent cancelled for " + event.getPlayer().getName() + ".");
+            return;
+        }
+
+        Debugger.write("PlayerFishListener: current state: " + event.getState());
 
         if (event.getState() == PlayerFishEvent.State.CAUGHT_FISH && event.getCaught() instanceof Item item) {
-            Debugger.write("=========================================================================================");
             Debugger.write("PlayerFishListener: onPlayerFishEvent summoned by " + event.getPlayer().getName() + " for " + item.getItemStack().getType() + ".");
 
             setPlayerQuestProgression(event, event.getPlayer(), 1, "FISH");
