e1a8db08d35e07b1d66b21a7949c5cc25cb0f7ed
Ordwen <samuelespallier77@gmail.com>
2025-10-03 22:04:50 +0200

    misc: fixed javadoc

--- src/main/java/com/ordwen/odailyquests/quests/conditions/placeholder/PlaceholderCondition.java
+++ src/main/java/com/ordwen/odailyquests/quests/conditions/placeholder/PlaceholderCondition.java
@@ -13,13 +13,13 @@ import com.ordwen.odailyquests.quests.conditions.ConditionOperator;
  *   <li>the expected value for comparison,</li>
  *   <li>and an optional error message to display if the condition fails.</li>
  * </ul>
- * Instances of this record are immutable.
- * </p>
  *
- * @param placeholder   the placeholder expression to evaluate (e.g. {@code "%player_level%"})
+ * <p>Instances of this record are immutable.
+ *
+ * @param placeholder       the placeholder expression to evaluate (e.g. {@code "%player_level%"})
  * @param conditionOperator the type of condition to check (e.g. equals, greater than, etc.)
- * @param expectedValue the expected value to compare against
- * @param errorMessage  the message to display if the condition fails (may be null or empty)
+ * @param expectedValue     the expected value to compare against
+ * @param errorMessage      the message to display if the condition fails (may be null or empty)
  */
 public record PlaceholderCondition(
         String placeholder,
